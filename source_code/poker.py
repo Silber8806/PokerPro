3a8763323af5464792d363ff522728fa69753744
Chris Kottmyer <Silber8806@gmail.com>
2021-02-10 13:29:45 -0500

    generate a sequence of card games

--- source_code/poker.py
+++ source_code/poker.py
@@ -5,6 +5,10 @@ import collections
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
+def chunk(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
 class PokerGame():
     def __init__(self,players):
         print('started poker game')
@@ -27,4 +31,35 @@ class Deck():
 
     def original_deck(self):
         return self.removed_cards + self.cards
+
+    def shuffle_deck(self):
+        self.cards = self.original_deck()
+        self.removed_cards = []
+        random.shuffle(self.cards)
+        return None
+
+class DeckSequence():
+    def __init__(self,players,decks=10):
+        self.players = players
+        self.decks=decks
+        self.cards_in_play = players * 2 + 5
+        self.cards_used_per_deck = 52 - (52 % self.cards_in_play)
+        self.plays_per_deck = self.cards_used_per_deck / self.cards_in_play
+
+        sample_deck = Deck()
+
+        self.sequence = []
+
+        for _ in range(0,self.decks):
+            generated_deck = sample_deck.cards[0:self.cards_used_per_deck]
+            self.sequence.extend(generated_deck)
+            sample_deck.shuffle_deck()
+
+        self.hands = list(chunk(self.sequence,self.cards_in_play))
+
+if __name__ == '__main__':
+    deck = DeckSequence(players=2,decks=100)
+
+    for hand in deck.hands:
+        print(hand)
 
