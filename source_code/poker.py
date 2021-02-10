4ab353900a1511ceb47c9bd389705d7f2796e68f
Chris Kottmyer <Silber8806@gmail.com>
2021-02-10 12:51:34 -0500

    adding deck you can use to pull cards based on fluent python

--- source_code/poker.py
+++ source_code/poker.py
@@ -1,6 +1,30 @@
+#!/usr/bin/env python3
 
+import random
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class PokerGame():
     def __init__(self,players):
         print('started poker game')
         self.players = players
+
+class Deck():
+    def __init__(self):
+        self.ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+        self.suits = suits = 'spades diamonds clubs hearts'.split()
+        standard_card_deck = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+        random.shuffle(standard_card_deck)
+        self.cards = standard_card_deck
+        self.removed_cards = []
+
+    def draw(self,number_of_cards):
+        drawn_cards = self.cards[0:number_of_cards]
+        self.cards = self.cards[number_of_cards:]
+        self.removed_cards.append(drawn_cards)
+        return drawn_cards
+
+    def original_deck(self):
+        return self.removed_cards + self.cards
+
