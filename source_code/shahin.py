b1478d11c481d9de8be85370a0fc170b0195c16a
Shirazi <Shahin.Shirazi@p66.com>
2021-02-14 18:34:40 -0600

    added straight function to pick straight and straight flush hands

--- source_code/shahin.py
+++ source_code/shahin.py
@@ -9,7 +9,10 @@ RankMap = {rank:i+1 for i, rank in enumerate([str(n) for n in range(2, 11)] + li
 
 def is_flush(cards):
     "This function check each players hand for flush and returns dictionary of flush and the suit"
-    card_suit=list()
+    if len(cards)<5: 
+        raise Exception("Need at least 5 cards to check for flush")
+        return 0
+    card_suit=[]
     for card in cards:
         card_suit.append(card.suit)
     card_suits_set=list(Counter(card_suit).keys())
@@ -17,8 +20,69 @@ def is_flush(cards):
     if max(card_suits_counter)>=5:
         flush_count=max(card_suits_counter)
         flush_suit=card_suits_set[card_suits_counter.index(flush_count)]
-        return {'flush':flush_suit}
+        flush_cards=[]
+        flush_rank=[]
+        for card in cards:
+            if card.suit==flush_suit:
+                flush_cards.append(card)
+                #Find highes flush
+                flush_rank.append(Ranks.index(card.rank))
+        return {'flush':{'suit':flush_suit,'flush_cards':flush_cards,'High_flush_on':Ranks[max(flush_rank)]}}
 
+
+def is_straight(cards):
+    #"This function check for straight and straight flush"
+    #"It returns highes straight flush if it exist, in case there is no straight flush, it will return highest straight if it exists"
+    straight_flush=False
+    straight={}
+    card_rank=[]
+    card_suit=[]
+    for card in cards:
+        card_rank.append(Ranks.index(card.rank))
+        card_suit.append(card.suit)
+    sort_rank=sorted(set(card_rank))
+    print('sorted ranking is=',sort_rank)
+    #check for lowest possible rank with Ace counting as 1
+    if max(sort_rank)==12:
+        if sort_rank[3]==3:
+            straight_cards=[]
+            for card in cards:
+                if Ranks.index(card.rank) in [0,1,2,3,12]:
+                    straight_cards.append(card)
+            print('straight_cards hand is=', straight_cards)
+            if is_flush(straight_cards):
+                straight_flush=True
+                straight={'straigh_flush':{'suit':is_flush(straight_cards)['flush']['suit'],'High_straight_on':5}}
+            else: 
+                straight={'High_straight_on':5}
+
+    #iterating over each set of card with 5cards to check for straight
+    card_index=0
+    while len(sort_rank)-card_index>=5:
+        #checking if we have straight flush or not.
+        if sort_rank[card_index+4]-sort_rank[card_index]==4:
+            straight_cards=[]
+            for card in cards:
+                if Ranks.index(card.rank) in sort_rank[card_index:card_index+5]:
+                    straight_cards.append(card)
+            print('straight_cards hand is=', straight_cards)
+            if is_flush(straight_cards):
+                straight_flush=True
+                straight={'straigh_flush':{'suit':is_flush(straight_cards)['flush']['suit'],'High_straight_on':Ranks[sort_rank[card_index+4]]}}
+            else: 
+                if straight_flush==False:# Dont count straight if you had straight flush
+                    straight={'High_straight_on':Ranks[sort_rank[card_index+4]]}
+        card_index+=1
+    return straight
+
+
+    
+
+    if len(cards)<5: 
+        raise Exception("Need at least 5 cards to check for flush")
+        return 0
+
+    return 0
 
 
 def score_hand(cards):
@@ -28,10 +92,18 @@ def score_hand(cards):
         return 0
     print("testing the score")
     if is_flush(cards):
-        print("\n\n\n******************************Congratulations********************\n\n\n")
-        print("This set of card is flash on ",flush_suit)
+        print("\n\n\n******************************Congratulations**flush******************\n\n\n")
+        print("This set of card is flash on ",is_flush(cards))
     else:
         print("no Flush")
+    straight=is_straight(cards)
+    if straight:
+        if 'straight_flush' in straight.keys():
+            print("\n\n\n******************************Congratulations**straight**flush****************\n\n\n")
+            print("This set of card is flash on ",straight['straight_flush']['suit'],"and straight high on",straight['straight_flush']['High_straight_on'])
+        else:
+            print("\n\n\n******************************Congratulations**straight******************\n\n\n")
+            print("This set of card is high straight on ",straight['High_straight_on'])
 
 if __name__ == '__main__':
     '''
