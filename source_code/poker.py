25fda8be63f0356572a7cf1aa2bf0e1b41d0e698
Shirazi <Shahin.Shirazi@p66.com>
2021-02-21 20:29:07 -0600

    add missing =  for player_score=[] on line 535

--- source_code/poker.py
+++ source_code/poker.py
@@ -6,6 +6,7 @@ import csv
 import random
 import collections
 import time
+from collections import Counter
 
 # named tuple is a tuple object (immutable type) that also has names.  
 # immutable means once created they can't be modified.
@@ -262,26 +263,290 @@ def simulate_win_odds(cards,river,opponents,runtimes=1000):
 
     return wins/float(runtimes) # your percent wins
 
+#################Shahin's addition############################################
+def is_flush(cards):
+    """This function check each players hand for flush and returns dictionary 
+    of flush, highest card on flush and the suit"""
+    if len(cards)<5: 
+        raise Exception("Need at least 5 cards to check for flush")
+        return 0
+    card_suit=[]
+    for card in cards:
+        card_suit.append(card.suit)
+    card_suits_set=list(Counter(card_suit).keys())
+    card_suits_counter=list(Counter(card_suit).values())
+    if max(card_suits_counter)>=5:
+        flush_count=max(card_suits_counter)
+        flush_suit=card_suits_set[card_suits_counter.index(flush_count)]
+        flush_cards=[]
+        flush_rank=[]
+        for card in cards:
+            if card.suit==flush_suit:
+                flush_cards.append(card)
+                #Find highest flush
+                flush_rank.append(Ranks.index(card.rank))
+        return {'flush':{'suit':flush_suit,'flush_cards':flush_cards,'High_flush_on':max(flush_rank)}}
+
+
+def is_straight(cards):
+    """This function check for straight and straight flush
+    It returns highes straight flush if it exist, in case
+    there is no straight flush, it will return highest straight if it exists"""
+    straight_flush=False
+    straight={}
+    card_rank=[]
+    card_suit=[]
+    for card in cards:
+        card_rank.append(Ranks.index(card.rank))
+        card_suit.append(card.suit)
+    sort_rank=sorted(set(card_rank))
+    "need to have at least 5 different ranking to check for straight"
+    if len(sort_rank)>4:
+        #check for lowest possible rank with Ace counting as 1
+        if max(sort_rank)==12:
+            if sort_rank[3]==3:
+                straight_cards=[]
+                for card in cards:
+                    if Ranks.index(card.rank) in [0,1,2,3,12]:
+                        straight_cards.append(card)
+                if is_flush(straight_cards):
+                    straight_flush=True
+                    straight={'straight_flush':{'suit':is_flush(straight_cards)['flush']['suit'],'High_straight_on':5}}
+                else: 
+                    straight={'High_straight_on':5}
+
+        #iterating over each set of card with 5cards to check for straight
+        card_index=0
+        while len(sort_rank)-card_index>=5:
+            #checking if we have straight flush or not.
+            if sort_rank[card_index+4]-sort_rank[card_index]==4:
+                straight_cards=[]
+                for card in cards:
+                    if Ranks.index(card.rank) in sort_rank[card_index:card_index+5]:
+                        straight_cards.append(card)
+                print('straight_cards hand is=', straight_cards)
+                if is_flush(straight_cards):
+                    straight_flush=True
+                    straight={'straight_flush':{'suit':is_flush(straight_cards)['flush']['suit'],'High_straight_on':sort_rank[card_index+4]}}
+                else: 
+                    if straight_flush==False:# Dont count straight if you have straight flush
+                        straight={'High_straight_on':sort_rank[card_index+4]}
+            card_index+=1
+    return straight
+
+def is_fullHouse(cards):
+    'This function returns the fullhouse hand with its trips and pair cards'
+    card_rank=[]
+    high_fullhouse=[]
+    fullhouse_dic={}
+    for card in cards:
+        card_rank.append(card.rank)
+    rank_set=list(Counter(card_rank).keys())
+    rank_repetition=list(Counter(card_rank).values())
+    "getting numerical value of each rank for comparison"
+    numeric_rank_set=[Ranks.index(r) for r in rank_set]
+    sort_rank=sorted(numeric_rank_set)
+    fullhouse=[numeric_rank_set[i] for i,j in enumerate(rank_repetition) if (j==2 or j==3)]
+    threekind_list=[numeric_rank_set[i] for i,j in enumerate(rank_repetition) if j==3]
+    "checking condition for fullhouse"
+    if ((len(threekind_list)>0) & (len(fullhouse)>1)):
+        max_threekind=max(threekind_list)
+        "Adding highest three of a kind"
+        high_fullhouse.append(fullhouse.pop(fullhouse.index(max_threekind)))
+        "Adding the second highest card, can be three of the kind or two of the kind"
+        max_pair=max(fullhouse)
+        high_fullhouse.append(fullhouse.pop(fullhouse.index(max(fullhouse))))
+        fullhouse_dic={'Fullhouse_on':[Ranks[i] for i in high_fullhouse],'Trips_on':max_threekind,'Pairs_on':max_pair}
+    return fullhouse_dic
+    
+def number_of_kind(cards):
+    "This function returns highes number of a kind anywhere between 2 to 4 if there is less than 2 of a kind it returns highes card"
+    card_rank=[]
+    for card in cards:
+        card_rank.append(card.rank)
+    rank_set=list(Counter(card_rank).keys())
+    rank_repetition=list(Counter(card_rank).values())
+    "getting numerical value of each rank for comparison"
+    numeric_rank_set=[Ranks.index(r) for r in rank_set]
+    max_repetition=max(rank_repetition)
+    sort_rank=sorted(numeric_rank_set)
+    kicker_card=list()
+    if max_repetition==4:
+        """Returns 4 of a kind and use remaining 1 card for kicker-card in case they have the same 
+        4 of the kind"""
+        
+        "Find next highest card in case highest card is the same as 4 of a kind"
+        if Ranks[sort_rank[-1]]==rank_set[rank_repetition.index(max_repetition)]:
+            sort_rank.pop(-1)
+        kicker_card.append(sort_rank[-1])
+        return {'number_of_kind':4,'number_of_kind_on':rank_set[rank_repetition.index(max_repetition)],'kicker_card':kicker_card}
+    elif max_repetition==3:
+        "No need to check for possibilities of having two three of the kinds"        
+        three_kind=[numeric_rank_set[i] for i,j in enumerate(rank_repetition) if j==3]
+        assert len(three_kind)<3, "Can not have more than two sets of three of a kind"
+        if len(three_kind)>1:
+            #assert len(three_kind)>1,"Your code is working fine, but your logic not because if you have more than one three of the kind it should be full house"
+            high_three_card=max(three_kind)
+            return {'number_of_kind':0}
+        else:
+            high_three_card=three_kind[0]
+        counter=0
+        while len(kicker_card)<2:
+            "Going to next card if any of the two highest cards are the same as 3 of a kinds"
+            if Ranks[sort_rank[-1-counter]]==high_three_card:
+                sort_rank.pop(-1-counter)
+            kicker_card.append(sort_rank[-1-counter])
+            counter+=1
+        return {'number_of_kind':3,'number_of_kind_on':high_three_card,'kicker_card':kicker_card}
+    elif max_repetition==2:
+        #need to check how many pairs we have and then select the highest two pairs if there are more than one.
+        pair_rank=[]
+        for repeat_index in range(len(rank_repetition)):
+            if rank_repetition[repeat_index]==2:
+                pair_rank_index=Ranks.index(rank_set[repeat_index])
+                pair_rank.append(pair_rank_index)
+        number_of_pairs=len(pair_rank)
+        if number_of_pairs==1:
+            counter=0
+            while len(kicker_card)<3:
+                "Going to next card if any of the three highest cards are the same as 3 of a kinds"
+                if Ranks[sort_rank[-1-counter]]==rank_set[rank_repetition.index(max_repetition)]:
+                    sort_rank.pop(-1-counter)
+                kicker_card.append(sort_rank[-1-counter])
+                counter+=1
+            return {'number_of_kind':2,'number_of_pair':1,'number_of_kind_on':Ranks[pair_rank[0]],'sort_order':pair_rank,'kicker_card':kicker_card}
+        elif number_of_pairs==2:
+            counter=0
+            while len(kicker_card)<1:
+                "Going to next card if any of the highest cards are the same as 3 of a kinds"
+                while Ranks[sort_rank[-1-counter]] in [Ranks[pair] for pair in pair_rank]:
+                    sort_rank.pop(-1-counter)
+                kicker_card.append(sort_rank[-1-counter])            
+            return {'number_of_kind':2,'number_of_pair':2,'number_of_kind_on':[Ranks[(pair_rank[i])] for i in range(2)],'sort_order':sorted(pair_rank,reverse=True),'kicker_card':kicker_card}
+        else:
+            assert number_of_pairs<4, "Can not have more than 3 pairs"
+            pair_rank=sorted(pair_rank)
+            #removing lowest pair when we have three pair
+            pair_rank.pop(0)
+            counter=0
+            while len(kicker_card)<1:
+                "Going to next card if any of the highest cards are the same as 3 of a kinds"
+                while Ranks[sort_rank[-1-counter]] in [Ranks[pair] for pair in pair_rank]:
+                    sort_rank.pop(-1-counter)
+                kicker_card.append(sort_rank[-1-counter])            
+            return {'number_of_kind':2,'number_of_pair':2,'number_of_kind_on':[Ranks[(pair_rank[i])] for i in range(2)],'sort_order':sorted(pair_rank,reverse=True),'kicker_card':kicker_card}
+    else:
+        assert max_repetition==1, "number of repetition should be between 1 and 4"
+        kicker_card=sort_rank[-2::-1]
+        return {'number_of_kind':1,'highest_card_on':sort_rank[-1],'kicker_card':kicker_card}
+
+"returning hands scoure in terms of dictionary with their values being a list to make it easier to compare hand at the end"
+
+
+def score_hand(cards):
+    print("scoring: {}".format(cards))
+    """ This function scoring each hand and returning a list as score with priority score starting from item 0 in the score
+    for example the highest ranking of the hands given to straight flush, if two people get straight flush then it will look at
+    the secon item in the list which is the highest card on their straight flush.
+    poker_hierarchy dictionary is used to rank each hand.
+    """
+    poker_hierarchy={'high_card':1,'one_pair':2,'two_pair':3,'three_of_kind':4,'straight':5,'flush':6,'full_house':7,'four_of_kind':8,'straight_flush':9}
+    if (len(cards) != 7):
+        raise Exception("Can only score 7 cards")
+        return 0
+    straightFlush_or_straight=is_straight(cards)
+    flush=is_flush(cards)
+    fullHouse=is_fullHouse(cards)
+    of_a_kind=number_of_kind(cards)
+    straight_flush=[]
+    straight=[]
+    four_of_kind=[]
+    three_of_kind=[]
+    two_pair=[]
+    one_pair=[]
+    high_card=[]
+    if (straightFlush_or_straight):
+        try:
+            flush_list=straightFlush_or_straight['straight_flush']
+            straight_flush=[poker_hierarchy['straight_flush']]+[flush_list['High_straight_on']]
+        except:
+            straight=[poker_hierarchy['straight']]+[straightFlush_or_straight['High_straight_on']]
+    if of_a_kind:
+        if of_a_kind['number_of_kind']==4:
+            four_of_kind=[poker_hierarchy['four_of_kind']]+[of_a_kind['number_of_kind_on']]+of_a_kind['kicker_card']
+        elif of_a_kind['number_of_kind']==3:
+            three_of_kind=[poker_hierarchy['three_of_kind']]+[of_a_kind['number_of_kind_on']]+of_a_kind['kicker_card']
+        elif of_a_kind['number_of_kind']==2:
+            if len(of_a_kind['sort_order'])==2:
+                two_pair=[poker_hierarchy['two_pair']]+of_a_kind['sort_order']+of_a_kind['kicker_card']
+            else:
+                one_pair=[poker_hierarchy['one_pair']]+of_a_kind['sort_order']+of_a_kind['kicker_card']
+        elif of_a_kind['number_of_kind']==1:
+            high_card=[poker_hierarchy['high_card']]+[of_a_kind['highest_card_on']]+of_a_kind['kicker_card']
+        else:
+            print("number_of_kind function should return number of kind between 1 and 4, but it returned ",of_a_kind['number_of_kind'])
+
+    if (straight_flush):
+        #print("\n\n\n******************************Congratulations**straight**flush****************\n\n\n")
+        #print("This set of card is flash on ",straight_flush)
+        return straight_flush
+    elif four_of_kind:
+        #print("\n\n\n******************************Congratulations**four*of*a*kind****************\n\n\n")
+        return four_of_kind
+    elif fullHouse:
+        #print("\n\n\n******************************Congratulations**fullhouse****************\n\n\n")
+        return [poker_hierarchy['full_house']]+[fullHouse['Trips_on']]+[fullHouse['Pairs_on']]
+    elif flush:
+        #print("\n\n\n******************************Congratulations**flush******************\n\n\n")
+        #print("This set of card is flash on ",flush['flush']['High_flush_on'])
+        return [poker_hierarchy['flush']]+[flush['flush']['High_flush_on']]
+    elif straight:
+        #print("\n\n\n******************************Congratulations**straight******************\n\n\n")
+        #print("This set of card is high straight on ",straight)
+        return straight
+    elif three_of_kind:
+        #print("\n\n\n******************************Congratulations**three*of*a*kind****************\n\n\n")
+        return three_of_kind
+    elif two_pair:
+        #print("\n\n\n******************************Congratulations**two*pairs****************\n\n\n")
+        return two_pair
+    elif one_pair:
+        #print("\n\n\n******************************Congratulations**one*pair****************\n\n\n")
+        return one_pair
+    elif high_card:
+        #print("\n\n\n******************************Congratulations**high*card****************\n\n\n")
+        return high_card
+    else:
+        print('No ranking was found on this hand, check your score function')
+    
+    
+
+
+################end of functions defined by Shahin ###########################
+
 def winning_hand(*hands):
     """ 
         used in the simulate_win_odds function.  This needs to be implemented still.
         Right now it just randomly picks win or lose.  Use the hand score function
         here and than check if 1st entry has highest rank.
     """
+    winning_hand=[]
     hand_scores = []
+    player_scores=[]
     for hand in hands:
-        score = score_hand(hand) 
+        score = score_hand(hand)
+        if winning_hand<score:
+            winning_hand=score 
         hand_scores.append(score)
+    for player,hand in enumerate(hands):
+        if hand==winning_hands:
+            player_scores.append(1)
+        else:
+            player_scores.append(0)
     # if 1st hand did not win return 0 else 1
-    return random.choice([0,1])
+    return player_scores
 
-def score_hand(cards):
-    """ 
-        needs to be implemented.  You need to take the hand of 7 cards
-        rank it so that it can be compared to other hands.
-    """
-    #dprint("scoring: {}".format(cards))
-    return 0
+
 
 class GenericPlayer(object):
 
