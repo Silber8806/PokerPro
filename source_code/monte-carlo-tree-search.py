3a2dffd1e46ecee05cab76e893ee1635ee3ef77c
Chris Kottmyer <Silber8806@gmail.com>
2021-04-13 18:57:09 -0400

    holding

--- source_code/monte-carlo-tree-search.py
+++ source_code/monte-carlo-tree-search.py
@@ -47,11 +47,53 @@ class DecisionNode(PlayerNode):
     def __init__(self):
         super().__init__()
 
+class MCST_Set():
+    def __init__(self):
+        self.game_types = {}
+
+    def all_hands(self):
+        return list(self.pre_flops.keys())
+
+    def add_game(self,number_of_players):
+        if number_of_players not in self.game_types:
+            self.game_types[number_of_players] = MCST(number_of_players)
+        return None 
+
+    def get_game(self,number_of_players):
+        if number_of_players not in self.game_types:
+            self.add_game(number_of_players)
+        return self.game_types[number_of_players] 
+
+    def __repr__(self):
+        message = 'MCTS set:'
+        for MCTS in self.game_types.values():
+            message += '\n' + str(MCTS) 
+        return message
+
 class MCST(object):
-    def __init__(self,turn_order):
-        self.turn_order = turn_order
-        self.card_context = []
+    def __init__(self,number_of_players):
+        self.number_of_players = number_of_players
+        self.hand_context = None 
+        self.card_context = None
+        self.turn_order = None
         pass
+
+    def get_turn_order(self):
+        return self.turn_order
+
+    def set_turn_order(self,turn_order):
+        self.turn_order = turn_order
+        return None
+
+    def get_player_hand(self):
+        return self.hand_context + self.card_context
+
+    def get_hand_context(self):
+        return self.hand_context
+
+    def set_hand_context(self, hand_context):
+        self.hand_context = hand_context 
+        return None
 
     def get_card_context(self):
         return self.card_context
@@ -60,49 +102,31 @@ class MCST(object):
         self.card_context = cards 
         return None
 
-class MCST_Set():
-    def __init__(self):
-        self.pre_flops = {}
+    def query(self,query):
+        return None
 
-    def all_hands(self):
-        return list(self.pre_flops.keys())
-
-    def add_game(self,turn_order):
-        game_tuple = (turn_order)
-        if cards not in self.pre_flops:
-            self.pre_flops[game_tuple] = MCST(turn_order)
-        return None 
-
-    def get_game(self,turn_order):
-        game_tuple = (turn_order)
-        if cards not in self.pre_flops:
-            self.pre_flops[game_tuple] = MCST(turn_order)
-        return self.pre_flops[game_tuple] 
-
-    def __repr__(self):
-        message = 'MCTS set:'
-        for turn_order, hand in self.all_hands():
-            message += '\n{} - {}'.format(turn_order, hand)
-        return message
+    def __repr__(self): 
+        return 'MCTS with {} players'.format(number_of_players)
 
 query_set = [
         ('player', 'current', 'bet', -5), 
         ('player', 'opponent 1', 'bet', 0), 
-        ('card', Card(rank='2', suit='spades')), 
+        ('card', (Card(rank='2', suit='spades'), Card(rank='5', suit='diamonds'), Card(rank='9', suit='diamonds'))), 
         ('player', 'current', 'bet', 0), 
         ('player', 'opponent 1', 'bet', 0), 
-        ('card', Card(rank='3', suit='diamonds')), 
+        ('card', (Card(rank='2', suit='spades'), Card(rank='5', suit='diamonds'), Card(rank='9', suit='diamonds'), Card(rank='3', suit='diamonds'))), 
         ('player', 'current', 'bet', 0), 
         ('player', 'opponent 1', 'bet', 0), 
-        ('card', Card(rank='3', suit='hearts'))
+        ('card', (Card(rank='2', suit='spades'), Card(rank='5', suit='diamonds'), Card(rank='9', suit='diamonds'), Card(rank='3', suit='diamonds'), Card(rank='3', suit='hearts')))
     ]
 
 if __name__ == '__main__':
     print("starting MCTS")
+    number_of_players = 2
     turn_order = ('current', 'opponent 1')
     cards = (Card(rank='9', suit='spades'), Card(rank='A', suit='spades'))
 
-    hand_sets = MCST_Set()
-    hand_sets.add_game(turn_order)
-    current_MCST = hand_sets.get_game(turn_order)
-    print(current_MCST)
+    trees = MCST_Set()
+    trees.add_game(number_of_players)
+    current_MCST = trees.get_game(number_of_players)
+    print(trees)
