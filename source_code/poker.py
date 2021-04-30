901766bbdf179b193e10bdd908d1bf8b04bd6671
Chris Kottmyer <Silber8806@gmail.com>
2021-04-29 23:08:26 -0400

    fixing quick bug

--- source_code/poker.py
+++ source_code/poker.py
@@ -2397,7 +2397,10 @@ class MCST(object):
         for query in query_set:
             player, action_type, _ = query
             prev_node = node
-            node = node.relations[action_type]
+            if action_type in node.relations:
+                node = node.relations[action_type]
+            else:
+                node = None
             
             if node is None:
                 if len(prev_node.turn_context) == 1 or prev_node.end_game_node == True:
@@ -2814,8 +2817,8 @@ if __name__ == '__main__':
 
     # defines all the simulations we will run
     simulations = {
-       'tables': 5, # number of poker tables simulated
-       'hands': 10, # number of hands the dealer will player, has to be greater than 2
+       'tables': 10, # number of poker tables simulated
+       'hands': 50, # number of hands the dealer will player, has to be greater than 2
        'balance': 100000, # beginning balance in dollars, recommend > 10,000 unless you want player to run out of money
        'minimum_balance': 50, # minimum balance to join a table
        'simulations': [ # each dict in the list is a simulation to run    
